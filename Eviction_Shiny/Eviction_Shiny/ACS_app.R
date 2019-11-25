249ff852b6c497ce193fa6ae69986e8541a0a373
Allison <allison.c.shafer@gmail.com>
2019-11-25 10:33:26 -0500

    ACS - App

--- Eviction_Shiny/Eviction_Shiny/ACS_app.R
+++ Eviction_Shiny/Eviction_Shiny/ACS_app.R
@@ -13,6 +13,7 @@ library(shiny)
 library(lubridate)
 library(tidyverse)
 library(shiny)
+library(sf)
 
 # load in csv files
 
@@ -21,11 +22,15 @@ library(shiny)
 #unzip(data, overwrite = FALSE)
 
 # read in csv 
-eviction_by_state <- read_csv(".Eviction_Shiny/eviction_by_state.csv")
+eviction_by_state <- read_csv("./eviction_state_2010.csv")
+
+state_us_geo <- tigris::states(class= "sf")
 
 spatial_state <- as_Spatial(state_us_geo)
 
-state_eviction <- merge(spatial_state, eviction_state_2010, by = "GEOID", duplicateGeoms = TRUE)
+state_eviction <- merge(spatial_state, eviction_by_state, by = c("GEOID" = "GEOID"), duplicateGeoms = TRUE)
+
+state_eviction <- sp.na.omit(state_eviction, margin = 1)
 
 purPal <- colorQuantile("PuBuGn", domain = state_eviction$eviction_filing_rate, n = 6)
 blugr <- colorQuantile("YlGnBu", domain = state_eviction$pct_renter_occupied, n = 6)
@@ -34,69 +39,80 @@ blugr <- colorQuantile("YlGnBu", domain = state_eviction$pct_renter_occupied, n 
 popup_evic <- paste0("<strong>", state_eviction$name,
                      "<strong><br />Median HH Income: <strong>", state_eviction$median_household_income)
 
-map_maker <- function(x){
+map_maker <- function(x, y){
   if (x == "Eviction Filing Rate") {
     leaflet() %>% 
-      setView(-98.483330, 38.712046, zoom = 4) %>%
+      setView(-98.483330, 38.712046, zoom = 3) %>%
       addProviderTiles(providers$CartoDB.Positron) %>%
-      addPolygons(data = state_eviction, fillColor = ~purPal(state_eviction$eviction_filing_rate), 
+      addPolygons(data = y, fillColor = ~purPal(state_eviction$eviction_filing_rate), 
                   smoothFactor = 0.2, fillOpacity = .7, weight = 0.2,
                   popup = ~popup_evic) %>%
       addLegend(purPal,
                 values = state_eviction$eviction_filing_rate,
                 position = "bottomleft",
-                title = "Eviction Filing Rates")
+                title = "Eviction Filing <br/ > Rates")
   }else{
     leaflet() %>% 
-      setView(-98.483330, 38.712046, zoom = 4) %>%
+      setView(lng = -93.85, lat = 37.45, zoom = 3) %>%
       addProviderTiles(providers$CartoDB.Positron) %>%
-      addPolygons(data = state_eviction, 
+      addPolygons(data = y, 
                   fillColor = ~blugr(state_eviction$pct_renter_occupied),
                   smoothFactor = 0.2, fillOpacity = .7, weight = 0.2,
                   popup = ~popup_evic) %>%
       addLegend(blugr,
                 values = state_eviction$pct_renter_occupied,
                 position = "bottomleft",
-                title = "Percent Renter Occupied") }
+                title = "Percent Renter Occupied") }}
  
 
     ui <- fluidPage(
-
+        sidebarLayout(
+          sidebarPanel(
             selectInput(inputId = "year",
                         label = "Select a Year:",
-                        choices = unique(eviction_by_state$year),
-                        value = 2012,
-                        step = 1),
-          
+                        choices = unique(eviction_by_state$year)),
             radioButtons(inputId = "layer",
                         label = "Select a Dataset to View:",
                         choices = c("Eviction Filing Rate", 
                                        "Percent Renter Occupied")),
             selectInput(inputId = "state",
                         label = "Select a State:",
-                        choices = unique(eviction_by_state$name)),
+                        choices = unique(state_eviction$name))
+            ),
             mainPanel(
                 leafletOutput("map"))
-            )
+            ))
                                      
     
     # Define server logic required to draw a histogram
 
     server <- function(input, output, session) {
      
-    filtered_data <- reactive({
-      state_eviction %>%
-        filter(year == input$year)})
+    #filtered_data <- reactive({
+      #state_eviction[state_eviction$year == input$year, ]
+      
+      y <- reactive({
+        subset(state_eviction, state_eviction$year == input$year)
+      
+    })
       
       x <- reactive({
         input$layer
-      })
+      
+    })
+        
+      
       
     
     output$map <- renderLeaflet({
-      map_maker(x())
+      
+      leaflet() %>% 
+        setView(-98.483330, 38.712046, zoom = 4) %>%
+        addProviderTiles(providers$CartoDB.Positron)
+      
+      map_maker(x(), y())
+      
       })
-
 }
 
 # Run the application 
